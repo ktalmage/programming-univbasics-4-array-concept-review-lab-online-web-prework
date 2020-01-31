bf6dd82ad750629390d751454328c60cd22656f6
Kyle Talmage <ktalmage85@gmail.com>
2020-01-31 16:35:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,10 +17,11 @@ end
 def find_min_value(array)
 x = 0
   array.length.times { |index|
-    if array[index] < x
+    if array[index] <= x
       x = array[index]
   end
 }
 x
 end
 
+array = [1,2,3]
